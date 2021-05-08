4bf75dfb50e94ca86a410fd56cd3a93dfb0ba4b7
rizalanhari <rizalanhari6@gmail.com>
2021-05-08 21:03:04 +0700

    add js 12

---
+++ Week12/12.3/Assets/SnapshotTrigger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+public class SnapshotTrigger : MonoBehaviour
+{
+    public AudioMixerSnapshot snapshot;
+    public float crossfade;
+    private void OnTriggerEnter(Collider other)
+    {
+        snapshot.TransitionTo(crossfade);
+    }
+}
